c30bd6741320d4b27dbde4fd86f1c7312cf3dc59
cgeils25 <cgeils@email.sc.edu>
2025-02-21 15:32:03 -0500

    move main functions for frontend into dedicated js file

---
+++ frontend/main.js
@@ -0,0 +1,94 @@
+async function predict(canvasId) {
+    // take the id of the canvas, get the image, pass to backend, receive the prediction
+    const canvasData = document.getElementById(canvasId).toDataURL();
+
+    const response = await fetch('http://localhost:8000/predict', {
+        method: 'POST',
+        body: JSON.stringify({
+            image: canvasData
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const data = await response.json();
+    return data;
+}
+
+async function updatePlot(canvasId) {
+    // get make a prediction and then update the bar chart
+    const predict_response = await predict(canvasId);
+
+    predictions = predict_response.predictions;
+
+    labels = Object.keys(predictions);
+    probabilities = Object.values(predictions);
+
+    const data = [{
+        x: labels,
+        y: probabilities,
+        type: 'bar',
+    }];
+
+    const layout = {
+        yaxis: {
+            range: [0, 1],
+            title: 'Probability (Model Output)'
+        },
+        xaxis: {
+            title: 'Digit'
+        }
+    };
+
+    Plotly.newPlot('plotly-test', data, layout);
+}
+
+function initCanvas(canvasId) {
+    // initialize the canvas and add event listeners so you can draw on it
+    const canvas = document.getElementById(canvasId);
+    const ctx = canvas.getContext('2d');
+    let drawing = false;
+
+    canvas.addEventListener('mousedown', () => {
+        drawing = true;
+    });
+
+    canvas.addEventListener('mouseup', () => {
+        drawing = false;
+        ctx.beginPath();
+    });
+
+    canvas.addEventListener('mousemove', (event) => {
+        if (!drawing) return;
+        ctx.lineWidth = 10;
+        ctx.lineCap = 'round';
+        ctx.strokeStyle = 'black';
+
+        ctx.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+    });
+
+    let lastExecution = 0;
+    let delay = 250; // how long to wait before updating the plot again
+
+    canvas.addEventListener('mousemove', (event) => {
+        const now = Date.now();
+        if (now - lastExecution >= delay) {
+            updatePlot(canvasId);
+            lastExecution = now;
+        }
+    });
+}
+
+function clearCanvas(canvasId) {
+    // clear the canvas
+    const canvas = document.getElementById(canvasId);
+    const ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+initCanvas('canvas');
+updatePlot('canvas');
