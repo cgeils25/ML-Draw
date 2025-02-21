b3d42f517f67f944caf8bc91c296cbf419f137e6
cgeils25 <cgeils@email.sc.edu>
2025-02-21 15:19:58 -0500

    build fastapi app that takes request. For now, just returns random numbers as prediction

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,59 @@
-from fastapi import FastAPI
+import numpy as np
 
-app = FastAPI()
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+
+from contextlib import asynccontextmanager
+import json
+
+from typing import Annotated
+from pydantic import BaseModel
+
+import pickle
+
+def load_model(model_path: str):
+    pass
+
+# FastAPI app
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    with open('backend/app_parameters.json', 'r') as f:
+        args = json.load(f)
+    
+    model = load_model(args['model_path'])
+
+    yield
+
+    # put any cleanup code here (stuff to do right before the app shuts down)
+
+app = FastAPI(lifespan=lifespan)
+
+# this makes it so that the frontend can access the API
+origins = [
+    "http://127.0.0.1:5500" # not sure how you would know the origins you want to allow ahead of time. Can use a wildcard but that seems insecure?
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+class MNISTPredictionResponse(BaseModel):
+    """
+    A response model for an MNIST prediction
+    """
+    predictions: dict = {}
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/predict", response_model=MNISTPredictionResponse, status_code=200)
+async def predict() -> MNISTPredictionResponse:
+    """
+    Make a prediction for a given MNIST Image
+    """
+    return MNISTPredictionResponse(predictions={i: np.random.rand() for i in range(10)})
