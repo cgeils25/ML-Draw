8bfef26a167a1bc4428f9375641c09c624bdb630
cgeils25 <cgeils@email.sc.edu>
2025-02-22 02:16:43 -0500

    export backend and frontend PIDs for kill script later

--- run.sh
+++ run.sh
@@ -1,13 +1,15 @@
 export FRONTEND_PORT=5500
-export BACKEND_PORT=8000 # for now this can't be changed because the frontend is hardcoded to use this port
+export BACKEND_PORT=8000
+
+# start the backend. putting it in dev mode means it will reload whenever you change the code
+uv run fastapi dev --port $BACKEND_PORT backend/app.py &
+
+export BACKEND_PID=$!
 
 # start hosting the frontend
 uv run python -m http.server -d frontend $FRONTEND_PORT &
 
+export FRONTEND_PID=$!
+
 # open the frontend in the browser
-uv run python -m webbrowser http://127.0.0.1:$FRONTEND_PORT/frontend/index.html &
-
-# start the backend. putting it in dev mode means it will reload whenever you change the code
-uv run fastapi dev --port $BACKEND_PORT backend/app.py
-
-
+uv run python -m webbrowser http://127.0.0.1:$FRONTEND_PORT
