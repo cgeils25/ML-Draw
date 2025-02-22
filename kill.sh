17247b62e414047098a85c8047f92e41a2bc7fc0
cgeils25 <cgeils@email.sc.edu>
2025-02-22 02:42:00 -0500

    use dotenv to store environment variables instead of exporting

--- kill.sh
+++ kill.sh
@@ -1,2 +1,2 @@
-kill $FRONTEND_PID
-kill $BACKEND_PID
+kill $(dotenv get FRONTEND_PID)
+kill $(dotenv get BACKEND_PID)
--- start.sh
+++ start.sh
@@ -4,12 +4,12 @@ export BACKEND_PORT=8000
 # start the backend. putting it in dev mode means it will reload whenever you change the code
 uv run fastapi dev --port $BACKEND_PORT backend/app.py &
 
-export BACKEND_PID=$!
+dotenv set BACKEND_PID $!
 
 # start hosting the frontend
 uv run python -m http.server -d frontend $FRONTEND_PORT &
 
-export FRONTEND_PID=$!
+dotenv set FRONTEND_PID $!
 
 # open the frontend in the browser
 uv run python -m webbrowser http://127.0.0.1:$FRONTEND_PORT
