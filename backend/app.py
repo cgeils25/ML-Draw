48548e9805a7e4fe0f95a32ef90bf6fe24a818dc
cgeils25 <cgeils@email.sc.edu>
2025-02-22 07:21:36 -0500

    add support for prediction with lenet-5

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 import numpy as np
+import torch
 import os
 import time
+import matplotlib.pyplot as plt
 
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
@@ -17,11 +19,26 @@ import base64
 from PIL import Image
 import io
 
+# for the pytorch model
+from models.lenet import LeNet
+
+VALID_MODEL_TYPES = ['lenet', 'logistic_regression', 'random_forest']
+
 logging=False
 
-def load_model(model_path: str):
-    with open(model_path, 'rb') as f:
-        model = pickle.load(f)
+def load_model(model_path: str, model_type=None):
+    if model_type not in VALID_MODEL_TYPES:
+        raise ValueError(f"Invalid model type: {model_type}")
+
+    if model_type == 'lenet':
+        # not going to bother with GPU. I experimented a little and the time to move the data to the GPU for every single prediction is not worth it
+        model = LeNet()
+        model.load_state_dict(torch.load(model_path))
+        model.eval()
+
+    elif model_type == 'logistic_regression' or model_type == 'random_forest':
+        with open(model_path, 'rb') as f:
+            model = pickle.load(f)
     
     return model
 
@@ -39,11 +56,13 @@ async def lifespan(app: FastAPI):
     with open('backend/app_parameters.json', 'r') as f:
         args = json.load(f)
     
-    lr_model = load_model(args['logistic_regression_model_path'])
-    rf_model = load_model(args['random_forest_model_path'])
+    lr_model = load_model(args['logistic_regression_model_path'], model_type='logistic_regression')
+    rf_model = load_model(args['random_forest_model_path'], model_type='random_forest')
+    lenet_model = load_model(args['lenet_model_path'], model_type='lenet')
 
     app.state.lr_model = lr_model
     app.state.rf_model = rf_model
+    app.state.lenet_model = lenet_model
 
     yield
 
@@ -74,6 +93,8 @@ class MNISTPredictionResponse(BaseModel):
 async def root():
     return {"message": "Hello World"}
 
+
+
 @app.post("/predict", response_model=MNISTPredictionResponse, status_code=200)
 async def predict(image_base64_string: Annotated[str | None, Query()] = None) -> MNISTPredictionResponse:
     """
@@ -98,12 +119,15 @@ async def predict(image_base64_string: Annotated[str | None, Query()] = None) ->
         lr_probabilities = app.state.lr_model.predict_proba([x])[0]   
         rf_probabilities = app.state.rf_model.predict_proba([x])[0]
 
+        small_image_pt = torch.tensor(x).float().view(1, 1, 28, 28)
 
+        with torch.no_grad():
+            lenet_probabilities = torch.nn.functional.softmax(app.state.lenet_model(small_image_pt), dim=1).numpy()[0]
 
         return MNISTPredictionResponse(predictions={'lr': {label: prob for label, prob in zip(app.state.lr_model.classes_.tolist(), lr_probabilities)},
-                                                    'rf': {label: prob for label, prob in zip(app.state.rf_model.classes_.tolist(), rf_probabilities)}})
+                                                    'rf': {label: prob for label, prob in zip(app.state.rf_model.classes_.tolist(), rf_probabilities)},
+                                                    'lenet': {label: prob for label, prob in enumerate(lenet_probabilities.tolist())}})
                                    
     return MNISTPredictionResponse(predictions={'lr': {i: 0 for i in range(10)},
-                                                    'rf': {i: 0 for i in range(10)}})
-                                   
-                                   
+                                                    'rf': {i: 0 for i in range(10)},
+                                                    'lenet': {i: 0 for i in range(10)}})
