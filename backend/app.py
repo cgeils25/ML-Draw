8b9fd98406e1493bce54370ef8a3259331a80c53
cgeils25 <cgeils@email.sc.edu>
2025-02-22 02:14:15 -0500

    implement model loading and prediction

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import time
 
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
@@ -11,9 +12,26 @@ from typing import Annotated
 from pydantic import BaseModel
 
 import pickle
+import base64
+
+from PIL import Image
+import io
+
+logging=False
 
 def load_model(model_path: str):
-    pass
+    with open(model_path, 'rb') as f:
+        model = pickle.load(f)
+    
+    return model
+
+def get_image_from_base64_string(base64_string: str):  
+    png_data = base64.b64decode(base64_string.replace(' ', '+')) # this took a disgusting amount of time to figure out
+
+    image = Image.open(io.BytesIO(png_data))
+
+    return image
+
 
 # FastAPI app
 @asynccontextmanager
@@ -22,6 +40,8 @@ async def lifespan(app: FastAPI):
         args = json.load(f)
     
     model = load_model(args['model_path'])
+
+    app.state.model = model
 
     yield
 
@@ -53,8 +73,28 @@ async def root():
     return {"message": "Hello World"}
 
 @app.post("/predict", response_model=MNISTPredictionResponse, status_code=200)
-async def predict() -> MNISTPredictionResponse:
+async def predict(image_base64_string: Annotated[str | None, Query()] = None) -> MNISTPredictionResponse:
     """
     Make a prediction for a given MNIST Image
     """
-    return MNISTPredictionResponse(predictions={i: np.random.rand() for i in range(10)})
+    if logging:
+        with open('logs/backend_base64.log', 'a') as f:
+            f.write(image_base64_string + '\n')
+
+    if image_base64_string is not None:
+        
+        image = get_image_from_base64_string(image_base64_string)
+
+        small_img = np.array(image.resize((28, 28)))
+
+        x = small_img[:, :, 3]
+
+        x = x.flatten()
+
+        x = 255 - x
+
+        prediction = app.state.model.predict_proba([x])
+
+        return MNISTPredictionResponse(predictions={i: prediction[0][i] for i in range(10)})
+
+    return MNISTPredictionResponse(predictions={i: 0 for i in range(10)})
